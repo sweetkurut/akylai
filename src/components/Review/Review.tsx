6a6409afc9710eb053f106923825fa17fd27f10b
Mikhaylov-M <mikhaylov.m@outlook.com>
2024-06-03 14:09:07 +0600

    fix swiper slides

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,4 +1,15 @@
+'use client'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Keyboard, Navigation, Pagination } from "swiper/modules";
+import img1 from '@/public/images/review1.png'
+import img2 from '@/public/images/review2.png'
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import styles from "./Review.module.scss";
+import './swiper.scss'
+import Image from "next/image";
 
 export const Review = () => {
   return (
@@ -8,6 +19,52 @@ export const Review = () => {
           Здесь <span className={styles.review__span}>отзывы</span> тех, кто
           прошел мою терапию
         </h2>
+
+        <Swiper
+          modules={[Navigation, Keyboard]}
+          className={styles.swiper}
+          navigation
+          keyboard
+          loop
+          spaceBetween={50}
+          slidesPerView={3}
+        >
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img1} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img1} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img1} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img1} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img1} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img1} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="" className={styles.card}>
+              <Image src={img2} alt='video' priority={false} />
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   )
