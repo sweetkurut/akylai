72c415c06d44fade8f50198e2d710b800c8c5ac9
IsaRakhatbekov <isarahatbekov250@gmail.com>
2024-06-03 17:20:06 +0600

    video links

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -4,6 +4,12 @@ import { Keyboard, Navigation, Pagination } from "swiper/modules";
 import img1 from "@/public/images/review1.png";
 import img2 from "@/public/images/review2.png";
 import img3 from "@/public/images/review3.png";
+import img4 from "@/public/images/review4.png";
+import img5 from "@/public/images/review5.png";
+import img6 from "@/public/images/review6.png";
+import img7 from "@/public/images/review7.png";
+import img8 from "@/public/images/review8.png";
+import img9 from "@/public/images/review9.png";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -40,33 +46,84 @@ export const Review = () => {
           }}
         >
           <SwiperSlide>
-            <a href="" className={styles.card} target="_blank">
+            <a
+              href="https://youtu.be/qJhWmo1lF-c?si=ShogJNa-cBwpjy6a"
+              className={styles.card}
+              target="_blank"
+            >
               <Image src={img1} alt="video" priority={false} />
             </a>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="" className={styles.card} target="_blank">
+            <a
+              href="https://youtube.com/shorts/A1JVr4nOncE?si=DmsQnn0Qe3lBb9Yc"
+              className={styles.card}
+              target="_blank"
+            >
               <Image src={img2} alt="video" priority={false} />
             </a>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="" className={styles.card} target="_blank">
+            <a
+              href="https://youtube.com/shorts/NjtmpaRCvc8?si=G5FFUfnBcsenX-xB"
+              className={styles.card}
+              target="_blank"
+            >
               <Image src={img3} alt="video" priority={false} />
             </a>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="" className={styles.card} target="_blank">
-              <Image src={img2} alt="video" priority={false} />
+            <a
+              href="https://youtu.be/iMXczAnh-Jc?si=CnLGrLfY3o2OIATu"
+              className={styles.card}
+              target="_blank"
+            >
+              <Image src={img4} alt="video" priority={false} />
             </a>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="" className={styles.card} target="_blank">
-              <Image src={img1} alt="video" priority={false} />
+            <a
+              href="https://youtu.be/P7faK6iUV8M?si=BdjWK9OU4bzNqrU9"
+              className={styles.card}
+              target="_blank"
+            >
+              <Image src={img5} alt="video" priority={false} />
             </a>
           </SwiperSlide>
           <SwiperSlide>
-            <a href="" className={styles.card} target="_blank">
-              <Image src={img3} alt="video" priority={false} />
+            <a
+              href="https://youtu.be/c2PqtI3uAW4?si=2xPnbxpzwyThI0WO"
+              className={styles.card}
+              target="_blank"
+            >
+              <Image src={img6} alt="video" priority={false} />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="https://youtube.com/shorts/SmcJthJRM6s?si=UnspE2GLhOcsJfOv"
+              className={styles.card}
+              target="_blank"
+            >
+              <Image src={img7} alt="video" priority={false} />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="https://youtu.be/mrRtVcXhBDM?si=vLISlmLgkOssI_4K"
+              className={styles.card}
+              target="_blank"
+            >
+              <Image src={img8} alt="video" priority={false} />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a
+              href="https://youtu.be/mrRtVcXhBDM?si=vLISlmLgkOssI_4K"
+              className={styles.card}
+              target="_blank"
+            >
+              <Image src={img9} alt="video" priority={false} />
             </a>
           </SwiperSlide>
         </Swiper>
