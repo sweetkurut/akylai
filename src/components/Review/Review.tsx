caddd3407596dc4031ad3349216346c836091035
IsaRakhatbekov <isarahatbekov250@gmail.com>
2024-06-03 15:25:07 +0600

    swiper style

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,14 +1,15 @@
-'use client'
+"use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard, Navigation, Pagination } from "swiper/modules";
-import img1 from '@/public/images/review1.png'
-import img2 from '@/public/images/review2.png'
+import img1 from "@/public/images/review1.png";
+import img2 from "@/public/images/review2.png";
+import img3 from "@/public/images/review3.png";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
 import styles from "./Review.module.scss";
-import './swiper.scss'
+import "./swiper.scss";
 import Image from "next/image";
 
 export const Review = () => {
@@ -25,47 +26,51 @@ export const Review = () => {
           className={styles.swiper}
           navigation
           keyboard
-          loop
-          spaceBetween={50}
-          slidesPerView={3}
+          spaceBetween={10}
+          slidesPerView={1}
+          initialSlide={1}
+          centeredSlides={true}
+          breakpoints={{
+            500: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+          }}
         >
           <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img1} alt='video' priority={false} />
-            </div>
+            <a href="" className={styles.card} target="_blank">
+              <Image src={img1} alt="video" priority={false} />
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img1} alt='video' priority={false} />
-            </div>
+            <a href="" className={styles.card} target="_blank">
+              <Image src={img2} alt="video" priority={false} />
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img1} alt='video' priority={false} />
-            </div>
+            <a href="" className={styles.card} target="_blank">
+              <Image src={img3} alt="video" priority={false} />
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img1} alt='video' priority={false} />
-            </div>
+            <a href="" className={styles.card} target="_blank">
+              <Image src={img2} alt="video" priority={false} />
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img1} alt='video' priority={false} />
-            </div>
+            <a href="" className={styles.card} target="_blank">
+              <Image src={img1} alt="video" priority={false} />
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img1} alt='video' priority={false} />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div id="" className={styles.card}>
-              <Image src={img2} alt='video' priority={false} />
-            </div>
+            <a href="" className={styles.card} target="_blank">
+              <Image src={img3} alt="video" priority={false} />
+            </a>
           </SwiperSlide>
         </Swiper>
       </div>
     </section>
-  )
-}
+  );
+};
